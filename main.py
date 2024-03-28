79634192dccf799ccf2453eabe615f671deb12ae
Aref-Mal <aref.malekanian@mail.utoronto.ca>
2024-03-28 17:56:12 -0400

    Albert

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import json
 
+
 def extract_recipes(data):
+    """Extract the recipes"""
     with open(data, 'r') as f:
         file = json.load(f)
-    difficulty_so_far = set()
-    for lines in file:
-        if lines['rattings'] not in difficulty_so_far:
-            difficulty_so_far.add(lines['rattings'])
-    return difficulty_so_far
+        difficulty_so_far = []
+        for lines in file:
+            if lines['times'] not in difficulty_so_far:
+                difficulty_so_far.append(lines['times'])
+        return difficulty_so_far
+    # for lines in file:
+    #     if lines['nutrients'] != {}:
+    #         difficulty_so_far += 1
+    # return difficulty_so_far
