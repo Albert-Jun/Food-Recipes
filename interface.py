e9974f840a6cac818d9b422b3da70b905519e305
Aref-Mal <aref.malekanian@mail.utoronto.ca>
2024-04-03 18:00:23 -0400

    Sorted foods

--- interface.py
+++ interface.py
@@ -797,7 +797,7 @@ def run_game() -> None:
                 elif i == 4:
                     y += 530
                 if len(rec_food[i].item) > 50:
-                    text = rec_food[i].item[40:]
+                    text = f'{rec_food[i].item[40:]} ...'
                 else:
                     text = rec_food[i].item
                 text_color = (255, 255, 255)
@@ -1114,7 +1114,7 @@ if __name__ == '__main__':
     import python_ta
 
     python_ta.check_all(config={
-        'extra-imports': ['textwrap', 'webbrowser', 'io', 'pygame', 'requests', 'graphs'],
+        'extra-imports': ['textwrap', 'webbrowser', 'io', 'requests', 'pygame', 'graphs'],
         # the names (strs) of imported modules
         'allowed-io': ['untoggle', 'draw', 'run', 'run_game'],
         'max-line-length': 120
